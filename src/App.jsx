5394de885c225a9263396c0177596d5899908ec6
Tomi07 <tom.palmeyro@gmail.com>
2025-06-17 11:48:23 -0300

    Noveno commit del proyecto final

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Body from "./componentes/Body"
 import './componentes/Usuarios/Perfiles.css'
-import { BrowserRouter as Router } from "react-router-dom";
 
 
 import './App.css'
